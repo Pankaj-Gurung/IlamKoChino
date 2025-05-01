a37017ac9220ea70143dcb03cf3ccf8ebd1a9e20
Pankaj Gurung <grgpankaj004@gmail.com>
2025-05-01 21:58:35 +0545

    update: make changes in home page

--- client/src/pages/home/home.page.jsx
+++ client/src/pages/home/home.page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Landing Page</div>
+    <>
+    <div className='home-container'>
+      Home Page
+    </div>
+    </>
   )
 }
 
